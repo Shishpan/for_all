1eb1961003772d3fe739c11a623aeeebec539d21
Andrey Shishpanov <shishpanov2010@yandex.ru>
2015-10-22 23:22:17 +0300

    changed double files

--- double-max.ispc_knl.ll
+++ double-max.ispc_knl.ll
@@ -11,48 +11,6 @@ declare <16 x float> @llvm.x86.avx512.mask.loadu.ps.512(i8*, <16 x float>, i16) 
 ; Function Attrs: nounwind argmemonly
 declare void @llvm.x86.avx512.mask.storeu.ps.512(i8*, <16 x float>, i16) #2
 
-; Function Attrs: nounwind readnone
-define i32 @width___(<16 x i1> %__mask) #0 {
-allocas:
-  ret i32 16
-}
-
-; Function Attrs: nounwind
-define void @f_f___un_3C_unf_3E_un_3C_unf_3E_(float* noalias nocapture %RET, float* noalias %aFOO, <16 x i1> %__mask) #3 {
-allocas:
-  %aFOO_load_ptr2int_2void = bitcast float* %aFOO to i8*
-  %mask_i16.i = bitcast <16 x i1> %__mask to i16
-  %res.i = tail call <16 x float> @llvm.x86.avx512.mask.loadu.ps.512(i8* %aFOO_load_ptr2int_2void, <16 x float> zeroinitializer, i16 %mask_i16.i) #3
-  %aFOO_load_offset_load_to_double = fpext <16 x float> %res.i to <16 x double>
-  %mul__a_load = fmul <16 x double> %aFOO_load_offset_load_to_double, <double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00, double 2.000000e+00>
-  %a_0.i.i = shufflevector <16 x double> %aFOO_load_offset_load_to_double, <16 x double> undef, <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 4, i32 5, i32 6, i32 7>
-  %a_1.i.i = shufflevector <16 x double> %mul__a_load, <16 x double> undef, <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 4, i32 5, i32 6, i32 7>
-  %res_a.i.i = tail call <8 x double> @llvm.x86.avx512.mask.max.pd.512(<8 x double> %a_0.i.i, <8 x double> %a_1.i.i, <8 x double> zeroinitializer, i8 -1, i32 4) #3
-  %b_0.i.i = shufflevector <16 x double> %aFOO_load_offset_load_to_double, <16 x double> undef, <8 x i32> <i32 8, i32 9, i32 10, i32 11, i32 12, i32 13, i32 14, i32 15>
-  %b_1.i.i = shufflevector <16 x double> %mul__a_load, <16 x double> undef, <8 x i32> <i32 8, i32 9, i32 10, i32 11, i32 12, i32 13, i32 14, i32 15>
-  %res_b.i.i = tail call <8 x double> @llvm.x86.avx512.mask.max.pd.512(<8 x double> %b_0.i.i, <8 x double> %b_1.i.i, <8 x double> zeroinitializer, i8 -1, i32 4) #3
-  %res.i.i = shufflevector <8 x double> %res_a.i.i, <8 x double> %res_b.i.i, <16 x i32> <i32 0, i32 1, i32 2, i32 3, i32 4, i32 5, i32 6, i32 7, i32 8, i32 9, i32 10, i32 11, i32 12, i32 13, i32 14, i32 15>
-  %calltmp_to_float = fptrunc <16 x double> %res.i.i to <16 x float>
-  %ptr_i8.i = bitcast float* %RET to i8*
-  call void @llvm.x86.avx512.mask.storeu.ps.512(i8* %ptr_i8.i, <16 x float> %calltmp_to_float, i16 %mask_i16.i) #3
-  ret void
-}
-
-; Function Attrs: nounwind
-define void @result___un_3C_unf_3E_(float* noalias nocapture %RET, <16 x i1> %__mask) #3 {
-allocas:
-  %mask_i16.i = bitcast <16 x i1> %__mask to i16
-  %ptr_i8.i = bitcast float* %RET to i8*
-  call void @llvm.x86.avx512.mask.storeu.ps.512(i8* %ptr_i8.i, <16 x float> <float 2.000000e+00, float 4.000000e+00, float 6.000000e+00, float 8.000000e+00, float 1.000000e+01, float 1.200000e+01, float 1.400000e+01, float 1.600000e+01, float 1.800000e+01, float 2.000000e+01, float 2.200000e+01, float 2.400000e+01, float 2.600000e+01, float 2.800000e+01, float 3.000000e+01, float 3.200000e+01>, i16 %mask_i16.i) #3
-  ret void
-}
-
-; Function Attrs: nounwind readnone
-define i32 @width() #0 {
-allocas:
-  ret i32 16
-}
-
 ; Function Attrs: nounwind
 define void @f_f(float* noalias nocapture %RET, float* noalias nocapture readonly %aFOO) #3 {
 allocas:
@@ -73,14 +31,6 @@ allocas:
   ret void
 }
 
-; Function Attrs: nounwind
-define void @result(float* noalias nocapture %RET) #3 {
-allocas:
-  %ptrcast = bitcast float* %RET to <16 x float>*
-  store <16 x float> <float 2.000000e+00, float 4.000000e+00, float 6.000000e+00, float 8.000000e+00, float 1.000000e+01, float 1.200000e+01, float 1.400000e+01, float 1.600000e+01, float 1.800000e+01, float 2.000000e+01, float 2.200000e+01, float 2.400000e+01, float 2.600000e+01, float 2.800000e+01, float 3.000000e+01, float 3.200000e+01>, <16 x float>* %ptrcast, align 4, !filename !1, !first_line !7, !first_column !2, !last_line !7, !last_column !6
-  ret void
-}
-
 attributes #0 = { nounwind readnone }
 attributes #1 = { nounwind readonly argmemonly }
 attributes #2 = { nounwind argmemonly }
@@ -89,7 +39,7 @@ attributes #3 = { nounwind }
 !llvm.ident = !{!0}
 
 !0 = !{!"clang version 3.8.0 (trunk 250939)"}
-!1 = !{!"/export/users/shishpan/ispc/tests/double-max.ispc"}
+!1 = !{!"/double-max.ispc"}
 !2 = !{i32 5}
 !3 = !{i32 16}
 !4 = !{i32 34}
--- double-max.ispc_knl.s
+++ double-max.ispc_knl.s
@@ -1,96 +1,5 @@
 	.text
-	.file	"./tests/double-max.ispc_knl.ll"
-	.globl	width___
-	.align	16, 0x90
-	.type	width___,@function
-width___:                               # @width___
-# BB#0:                                 # %allocas
-	movl	$16, %eax
-	retq
-.Lfunc_end0:
-	.size	width___, .Lfunc_end0-width___
-
-	.section	.rodata.cst4,"aM",@progbits,4
-	.align	4
-.LCPI1_0:
-	.long	2147483648              # 0x80000000
-	.text
-	.globl	f_f___un_3C_unf_3E_un_3C_unf_3E_
-	.align	16, 0x90
-	.type	f_f___un_3C_unf_3E_un_3C_unf_3E_,@function
-f_f___un_3C_unf_3E_un_3C_unf_3E_:       # @f_f___un_3C_unf_3E_un_3C_unf_3E_
-# BB#0:                                 # %allocas
-	vpmovsxbd	%xmm0, %zmm0
-	vpandd	.LCPI1_0(%rip){1to16}, %zmm0, %zmm0
-	vptestmd	%zmm0, %zmm0, %k0 
-	kmovw	%k0, %eax
-	kmovw	%eax, %k1
-	vmovups	(%rsi), %zmm0 {%k1} {z}
-	vcvtps2pd	%ymm0, %zmm1
-	vextractf64x4	$2, %zmm0, %ymm0
-	vcvtps2pd	%ymm0, %zmm0
-	vaddpd	%zmm0, %zmm0, %zmm2
-	vaddpd	%zmm1, %zmm1, %zmm3
-	vmaxpd	%zmm3, %zmm1, %zmm1
-	vmaxpd	%zmm2, %zmm0, %zmm0
-	vcvtpd2ps	%zmm1, %ymm1
-	vcvtpd2ps	%zmm0, %ymm0
-	vinsertf64x4	$1, %ymm0, %zmm1, %zmm0
-	vmovups	%zmm0, (%rdi) {%k1}
-	retq
-.Lfunc_end1:
-	.size	f_f___un_3C_unf_3E_un_3C_unf_3E_, .Lfunc_end1-f_f___un_3C_unf_3E_un_3C_unf_3E_
-
-	.section	.rodata.cst4,"aM",@progbits,4
-	.align	4
-.LCPI2_0:
-	.long	2147483648              # 0x80000000
-	.section	.rodata,"a",@progbits
-	.align	64
-.LCPI2_1:
-	.long	1073741824              # float 2.000000e+00
-	.long	1082130432              # float 4.000000e+00
-	.long	1086324736              # float 6.000000e+00
-	.long	1090519040              # float 8.000000e+00
-	.long	1092616192              # float 1.000000e+01
-	.long	1094713344              # float 1.200000e+01
-	.long	1096810496              # float 1.400000e+01
-	.long	1098907648              # float 1.600000e+01
-	.long	1099956224              # float 1.800000e+01
-	.long	1101004800              # float 2.000000e+01
-	.long	1102053376              # float 2.200000e+01
-	.long	1103101952              # float 2.400000e+01
-	.long	1104150528              # float 2.600000e+01
-	.long	1105199104              # float 2.800000e+01
-	.long	1106247680              # float 3.000000e+01
-	.long	1107296256              # float 3.200000e+01
-	.text
-	.globl	result___un_3C_unf_3E_
-	.align	16, 0x90
-	.type	result___un_3C_unf_3E_,@function
-result___un_3C_unf_3E_:                 # @result___un_3C_unf_3E_
-# BB#0:                                 # %allocas
-	vpmovsxbd	%xmm0, %zmm0
-	vpandd	.LCPI2_0(%rip){1to16}, %zmm0, %zmm0
-	vptestmd	%zmm0, %zmm0, %k0 
-	kmovw	%k0, %eax
-	kmovw	%eax, %k1
-	vmovaps	.LCPI2_1(%rip), %zmm0   # zmm0 = [2.000000e+00,4.000000e+00,6.000000e+00,8.000000e+00,1.000000e+01,1.200000e+01,1.400000e+01,1.600000e+01,1.800000e+01,2.000000e+01,2.200000e+01,2.400000e+01,2.600000e+01,2.800000e+01,3.000000e+01,3.200000e+01]
-	vmovups	%zmm0, (%rdi) {%k1}
-	retq
-.Lfunc_end2:
-	.size	result___un_3C_unf_3E_, .Lfunc_end2-result___un_3C_unf_3E_
-
-	.globl	width
-	.align	16, 0x90
-	.type	width,@function
-width:                                  # @width
-# BB#0:                                 # %allocas
-	movl	$16, %eax
-	retq
-.Lfunc_end3:
-	.size	width, .Lfunc_end3-width
-
+	.file	"double-max.ispc_knl.ll"
 	.globl	f_f
 	.align	16, 0x90
 	.type	f_f,@function
@@ -109,39 +18,8 @@ f_f:                                    # @f_f
 	vinsertf64x4	$1, %ymm0, %zmm1, %zmm0
 	vmovups	%zmm0, (%rdi)
 	retq
-.Lfunc_end4:
-	.size	f_f, .Lfunc_end4-f_f
-
-	.section	.rodata,"a",@progbits
-	.align	64
-.LCPI5_0:
-	.long	1073741824              # float 2.000000e+00
-	.long	1082130432              # float 4.000000e+00
-	.long	1086324736              # float 6.000000e+00
-	.long	1090519040              # float 8.000000e+00
-	.long	1092616192              # float 1.000000e+01
-	.long	1094713344              # float 1.200000e+01
-	.long	1096810496              # float 1.400000e+01
-	.long	1098907648              # float 1.600000e+01
-	.long	1099956224              # float 1.800000e+01
-	.long	1101004800              # float 2.000000e+01
-	.long	1102053376              # float 2.200000e+01
-	.long	1103101952              # float 2.400000e+01
-	.long	1104150528              # float 2.600000e+01
-	.long	1105199104              # float 2.800000e+01
-	.long	1106247680              # float 3.000000e+01
-	.long	1107296256              # float 3.200000e+01
-	.text
-	.globl	result
-	.align	16, 0x90
-	.type	result,@function
-result:                                 # @result
-# BB#0:                                 # %allocas
-	vmovaps	.LCPI5_0(%rip), %zmm0   # zmm0 = [2.000000e+00,4.000000e+00,6.000000e+00,8.000000e+00,1.000000e+01,1.200000e+01,1.400000e+01,1.600000e+01,1.800000e+01,2.000000e+01,2.200000e+01,2.400000e+01,2.600000e+01,2.800000e+01,3.000000e+01,3.200000e+01]
-	vmovups	%zmm0, (%rdi)
-	retq
-.Lfunc_end5:
-	.size	result, .Lfunc_end5-result
+.Lfunc_end0:
+	.size	f_f, .Lfunc_end0-f_f
 
 
 	.ident	"clang version 3.8.0 (trunk 250939)"
